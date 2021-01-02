46f45df226f2889e4467c0f5fc4a0b0957e96aeb
Nour Haridy <nourharidy@gmail.com>
2021-01-02 05:05:25 +0200

    add exploit test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
 
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+        blockNumber: 11567956
+      }
+    }
+  }
 };
 
